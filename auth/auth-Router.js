c0c6cf688f1115e26e6211b04e752c3c34a13180
James Basile <jbasile145@gmail.com>
2019-04-03 19:13:32 -0400

    login endpoint complete, token created when logging in

--- auth/auth-Router.js
+++ auth/auth-Router.js
@@ -15,5 +15,39 @@ router.post('/register', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+router.post('/login', (req,res) => {
+    const {username, password } = req.body;
+
+    db('users')
+        .where({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+
+                res.status(200).json({
+                    message: `Welcome, ${user.username}`,
+                    token
+                })
+            } else {
+                res.status(401).json({ message: 'Invalid username or password' })
+            }
+        })
+        .catch(err => res.status(500).json(err));
+})
+
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    };
+    const secret = 'The secret is that there is no secret';
+    const options = {
+        expiresIn: '1d'
+    }
+
+    return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
