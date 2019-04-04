72c7196a214e35ccc93653cc047003adcf7fb8e3
James Basile <jbasile145@gmail.com>
2019-04-03 20:09:58 -0400

    added restricted middleware

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,15 @@
+
+//Will work on routing tomorrow
+
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+
+// const db = require('../database/knexConfig');
+
+// module.exports = (req, res, next) => {
+//     const token = req.headers.authorization;
+
+//     if (token) {
+//         jwt.verify(token, secret)
+//     }
+// }
--- users/users-Router.js
+++ users/users-Router.js
@@ -1,12 +1,32 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const db = require('../database/knexConfig');
 
-router.get('/', (req, res) => {
+router.get('/', restricted, (req, res) => {
     db('users')
         .then(list => res.status(200).json(list))
         .catch(err => res.status(500).json(err));
 })
+
+
+
+function restricted(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: 'You shall not pass!' })
+            } else {
+                req.decodedJwt = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ message: 'You shall not pass!' })
+    }
+}
 
 
 
